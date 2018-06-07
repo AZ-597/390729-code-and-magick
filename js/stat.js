05c8d17c2034f0dbafa00ff28cf3adc4af3fdf78
Andy Z <az-597@yandex.ru>
2018-06-07 22:53:13 +0300

    Отрефакторил, вычистил комменты

--- js/stat.js
+++ js/stat.js
@@ -20,51 +20,44 @@ var getMaxResult = function (array) {
   return maxResult;
 };
 
-
-window.renderStatistics = function (ctx, names, times) {
-  // Рисуем тень белого облака
-  ctx.fillStyle = 'rgba(0, 0, 0, .7)';
-  ctx.fillRect(110, 20, CLOUD_WIDTH, CLOUD_HEIGHT);
-  ctx.strokeStyle = 'rgba(0, 0, 0, 1)';
-  ctx.strokeRect(110, 20, CLOUD_WIDTH, CLOUD_HEIGHT);
-  // Рисуем белое облако
-  ctx.fillStyle = 'rgba(255, 255, 255, 1)';
-  ctx.fillRect(100, 10, CLOUD_WIDTH, CLOUD_HEIGHT);
-  ctx.strokeStyle = 'rgba(0, 0, 0, 1)';
-  ctx.strokeRect(100, 10, CLOUD_WIDTH, CLOUD_HEIGHT);
-  // Рисуем заголовок
+var drawText = function (ctx, text, x, y) {
   ctx.fillStyle = 'rgba(0, 0, 0, 1)';
   ctx.strokeStyle = 'rgba(0, 0, 0, 1)';
   ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', 120, 45);
-  ctx.fillText('Список результатов:', 120, 65);
-  ctx.strokeText('Ура вы победили!', 120, 45);
-  ctx.strokeText('Список результатов:', 120, 65);
-  // Точка отсчёта для столбиков диаграммы
+  ctx.fillText(text, x, y);
+  ctx.strokeText(text, x, y);
+};
+
+var drawRect = function (ctx, fillColor, strokeColor, x, y, width, height) {
+  ctx.fillStyle = fillColor;
+  ctx.fillRect(x, y, width, height);
+  ctx.strokeStyle = strokeColor;
+  ctx.strokeRect(x, y, width, height);
+};
+
+
+window.renderStatistics = function (ctx, names, times) {
+  drawRect(ctx, 'rgba(0, 0, 0, .7)', 'rgba(0, 0, 0, 1)', 110, 20, CLOUD_WIDTH, CLOUD_HEIGHT);
+  drawRect(ctx, 'rgba(255, 255, 255, 1)', 'rgba(0, 0, 0, 1)', 100, 10, CLOUD_WIDTH, CLOUD_HEIGHT);
+
+  drawText(ctx, 'Ура вы победили!', 120, 45);
+  drawText(ctx, 'Список результатов:', 120, 65);
+
   var barPositionX = 150;
-  // Находим максимальный результат
+
   var maxResult = getMaxResult(times);
-  // Перебираем победителей, рисуем статистику игроков
+
   for (var i = 0; i < names.length; i++) {
-    // Рисуем имена
-    ctx.fillStyle = 'rgba(0, 0, 0, 1)';
-    ctx.strokeStyle = 'rgba(0, 0, 0, 1)';
-    ctx.font = '16px PT Mono';
-    ctx.fillText(names[i], barPositionX, 260);
-    ctx.strokeText(names[i], barPositionX, 260);
-    // Определем цвет столбика
+    drawText(ctx, names[i], barPositionX, 260);
+
     var barColor = names[i] !== 'Вы' ? 'rgba(0, 0, 255, ' + getRandom(1, 0.4) + ')' : 'rgba(255, 0, 0, 1)';
-    // Вычисляем высоту столбика
+
     var barHeight = (BAR_MAX_HEIGHT * times[i]) / maxResult;
-    // Рисуем столбик
+
     ctx.fillStyle = barColor;
     ctx.fillRect(barPositionX, 240, BAR_WIDTH, -1 * barHeight);
-    // Рисуем очки
-    ctx.fillStyle = 'rgba(0, 0, 0, 1)';
-    ctx.strokeStyle = 'rgba(0, 0, 0, 1)';
-    ctx.font = '16px PT Mono';
-    ctx.fillText(Math.round(times[i]), barPositionX, 240 - barHeight - 5);
-    ctx.strokeText(Math.round(times[i]), barPositionX, 240 - barHeight - 5);
+
+    drawText(ctx, Math.round(times[i]), barPositionX, 240 - barHeight - 5);
 
     barPositionX += (BAR_WIDTH + BAR_GAP);
   }
